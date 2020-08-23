fad6f832aa51de0ba1770b784f84d9e4a8cd9540
chimanDev <39435655+Chiman-Dev@users.noreply.github.com>
2020-08-23 12:04:19 +0530

    Add files via upload

---
+++ APIDetails/APIHeader.py
@@ -0,0 +1,7 @@
+"""
+This class is is used to mention all the API Headers\
+
+"""
+
+class ApiHeader:
+    headers = {"Content-Type": "application/json","charset" : "UTF-8"}
---
+++ APIDetails/APIURI.py
@@ -0,0 +1,15 @@
+"""
+This class is is used to mention all the API end points
+this con be optimized to run APi in different env, as of now its hard coded
+
+"""
+
+class ApiDetails:
+
+    fetchRecords1 = "https://jsonplaceholder.typicode.com/posts"
+    fetchRecords2="https://jsonplaceholder.typicode.com/posts"
+    fetchRecords3="https://jsonplaceholder.typicode.com/invalidposts"
+    postRecords="https://jsonplaceholder.typicode.com/posts"
+    putRecords="https://jsonplaceholder.typicode.com/posts/1"
+    deleteRecords="https://jsonplaceholder.typicode.com/posts/1"
+
